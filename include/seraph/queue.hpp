7b9117b6eef14b5dd78067f0b974d97fd647c494
Colin <34983726+colalb1@users.noreply.github.com>
2026-02-21 16:36:18 -0600

    Update formatting.

--- include/seraph/queue.hpp
+++ include/seraph/queue.hpp
@@ -4,33 +4,27 @@
 #include <stdexcept>
 #include <vector>
 
-namespace seraph
-{
+namespace seraph {
 
-    class Queue
-    {
+    class Queue {
       public:
         using value_type = int;
 
         Queue() = default;
 
-        void enqueue(value_type value)
-        {
+        void enqueue(value_type value) {
             storage_.push_back(value);
         }
 
-        value_type dequeue()
-        {
-            if (empty())
-            {
+        value_type dequeue() {
+            if (empty()) {
                 throw std::out_of_range("Cannot dequeue from an empty queue");
             }
 
             value_type value = storage_[head_];
             ++head_;
 
-            if (head_ > kCompactionThreshold && head_ * 2 > storage_.size())
-            {
+            if (head_ > kCompactionThreshold && head_ * 2 > storage_.size()) {
                 storage_.erase(
                         storage_.begin(),
                         storage_.begin() + static_cast<std::ptrdiff_t>(head_)
@@ -41,23 +35,19 @@ namespace seraph
             return value;
         }
 
-        [[nodiscard]] value_type front() const
-        {
-            if (empty())
-            {
+        [[nodiscard]] value_type front() const {
+            if (empty()) {
                 throw std::out_of_range("Cannot read front of an empty queue");
             }
 
             return storage_[head_];
         }
 
-        [[nodiscard]] bool empty() const noexcept
-        {
+        [[nodiscard]] bool empty() const noexcept {
             return head_ >= storage_.size();
         }
 
-        [[nodiscard]] std::size_t size() const noexcept
-        {
+        [[nodiscard]] std::size_t size() const noexcept {
             return storage_.size() - head_;
         }
 
