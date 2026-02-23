9c212a809cdacafa267612e0a189abb7de5c977a
Colin <colin.albs@gmail.com>
2026-02-22 20:17:17 -0600

    Creating three different classes to see which will be the fastest.

--- include/seraph/stack.hpp
+++ include/seraph/stack.hpp
@@ -4,8 +4,93 @@
 #include <stdexcept>
 #include <vector>
 
+// Need three versions: atomic, spinlock, compare-and-swap
 namespace seraph {
-    template <typename T> class Stack {
+    template <typename T> class AtomicStack {
+      private:
+        std::vector<T> storage_;
+
+      public:
+        Stack() = default;
+
+        void push(T value) {
+            storage_.push_back(value);
+        }
+
+        T pop() {
+            if (storage_.empty()) {
+                throw std::out_of_range("Cannot pop from an empty stack");
+            }
+
+            T value = storage_.back();
+            storage_.pop_back();
+            return value;
+        }
+
+        [[nodiscard]] T top() const {
+            if (storage_.empty()) {
+                throw std::out_of_range("Cannot read top of an empty stack");
+            }
+
+            return storage_.back();
+        }
+
+        [[nodiscard]] bool empty() const noexcept {
+            return storage_.empty();
+        }
+
+        [[nodiscard]] std::size_t size() const noexcept {
+            return storage_.size();
+        }
+
+        // TODO(colin): Template this container so Stack supports generic value types.
+        // TODO(colin): Add custom allocator support if performance goals require it.
+        // TODO(colin): Evaluate small-buffer optimization to reduce heap allocations.
+    };
+
+    template <typename T> class SpinlockStack {
+      private:
+        std::vector<T> storage_;
+
+      public:
+        Stack() = default;
+
+        void push(T value) {
+            storage_.push_back(value);
+        }
+
+        T pop() {
+            if (storage_.empty()) {
+                throw std::out_of_range("Cannot pop from an empty stack");
+            }
+
+            T value = storage_.back();
+            storage_.pop_back();
+            return value;
+        }
+
+        [[nodiscard]] T top() const {
+            if (storage_.empty()) {
+                throw std::out_of_range("Cannot read top of an empty stack");
+            }
+
+            return storage_.back();
+        }
+
+        [[nodiscard]] bool empty() const noexcept {
+            return storage_.empty();
+        }
+
+        [[nodiscard]] std::size_t size() const noexcept {
+            return storage_.size();
+        }
+
+        // TODO(colin): Template this container so Stack supports generic value types.
+        // TODO(colin): Add custom allocator support if performance goals require it.
+        // TODO(colin): Evaluate small-buffer optimization to reduce heap allocations.
+    };
+
+    template <typename T> class CASStack {
       private:
         std::vector<T> storage_;
 
